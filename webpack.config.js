d37eaf4933c4158b5dece25f011aa489a834a366
Juan Luis Rojas Leon <rojasleonjuan122@gmail.com>
2018-04-20 22:55:52 -0500

    Weback already with es6

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: './index.html',
+})
+
+const config = {
+  entry: [
+    'babel-polyfill',
+    'whatwg-fetch',
+    './src/index.js'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  stats: {
+    colors: true,
+    modules: true,
+    reasons: true,
+    errorDetails: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          // 'postcss-loader',
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)(\?v=.+)?$/,
+        loader: 'file-loader?name=assets/[name].[hash].[ext]',
+        options: {
+          name: '[path][name].[ext]',
+          outputPath: 'images/',
+        },
+      },
+    ]
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  plugins: [
+    htmlWebpackPlugin
+  ]
+}
+
+if(process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new config.optimization.minimize()
+  )
+}
+module.exports = config
