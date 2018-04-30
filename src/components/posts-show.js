f8e31ea7e27d7b5a52228b2ac5de63be2cfa7dc0
Juan Luis Rojas Leon <rojasleonjuan122@gmail.com>
2018-04-29 23:31:01 -0500

    only a post

--- src/components/posts-show.js
+++ src/components/posts-show.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions/index';
 
 class PostsShow extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchPost(id);
+  }
   render() {
     return (
       <div>
@@ -9,4 +15,7 @@ class PostsShow extends Component {
     );
   }
 }
-export default PostsShow;
+function mapStateToProps({ posts }, ownProps) {
+  return { post: posts[ownProps.match.params.id] };
+}
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
