84c9819e2388235d0a41be4987f70840d08ce06d
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-05-29 20:33:15 -0500

    Update project

--- src/components/posts-index.js
+++ src/components/posts-index.js
@@ -1,9 +1,9 @@
-import React, { Component, Fragment } from 'react';
-import { connect } from 'react-redux';
-import _ from 'lodash';
-import { Link } from 'react-router-dom';
+import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
+import _ from "lodash";
+import { Link } from "react-router-dom";
 
-import { fetchPosts } from '../actions/index';
+import { fetchPosts } from "../actions/index";
 
 class PostsIndex extends Component {
   componentDidMount() {
@@ -14,28 +14,31 @@ class PostsIndex extends Component {
     return (
       <div>
         <Fragment>
-          {!posts
-            ? <div className="loading">Loading inital data...</div>
-            : <div className="container">
-                <div className="header">
-                  <h2 className="posts">Posts</h2>
-                  <Link className="add-post" to="/posts/new">
-                    > Add a Post
-                  </Link>
-                </div>
-                <ul className="posts-list">
-                  {
-                    _.map(this.props.posts, (post) => {
-                      return <li style={{ marginBottom: '40px' }} key={post.id}>
-                              <Link className="post-link" to={`/posts/${post.id}`}>{post.title}</Link>
-                              <p className="categorie">{post.categories.toUpperCase()}</p>
-                              <p className="content">{post.content}</p>
-                             </li>;
-                    })
-                  }
-                </ul>
+          {!posts ? (
+            <div className="loading">Loading inital data...</div>
+          ) : (
+            <div className="container">
+              <div className="header">
+                <h2 className="posts">Posts</h2>
+                <Link className="add-post" to="/posts/new">
+                  > Add a Post
+                </Link>
               </div>
-          }
+              <ul className="posts-list">
+                {_.map(this.props.posts, post => {
+                  return (
+                    <li style={{ marginBottom: "40px" }} key={post.id}>
+                      <Link className="post-link" to={`/posts/${post.id}`}>
+                        {post.title}
+                      </Link>
+                      <p className="categorie">{post.categories}</p>
+                      <p className="content">{post.content}</p>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          )}
         </Fragment>
       </div>
     );
@@ -43,7 +46,10 @@ class PostsIndex extends Component {
 }
 function mapStateToProps(state) {
   return {
-    posts: state.posts,
+    posts: state.posts
   };
 }
-export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
+export default connect(
+  mapStateToProps,
+  { fetchPosts }
+)(PostsIndex);
